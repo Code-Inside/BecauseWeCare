2afe81bcf20b59b3eeae8cdfd49c6f900ee9cbf2
Robert Muehsig <robert.muehsig@code-developer.de>
2013-05-09 19:17:26 +0200

    init

---
+++ BecauseWeCare.Prototype/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raven.Abstractions.Data;
+using Raven.Client.Document;
+
+namespace BecauseWeCare.Prototype
+{
+    class Program
+    {
+        static string API_KEY = ""; // Needed!
+
+        static void Main(string[] args)
+        {
+
+            Site site = new Site();
+            site.Id = "110705";
+            site.Name = "Windows Phone Dev Platform";
+            site.Subdomain = "wpdev";
+            site.Url = "http://wpdev.uservoice.com/forums/110705-dev-platform";
+
+            SyncUserVoiceSiteToRavenDb(site, "http://localhost:8080/databases/feedbacks");
+
+        }
+
+        public static void SyncUserVoiceSiteToRavenDb(Site site, string ravenDbServer)
+        {
+            Console.WriteLine("SyncUserVoiceSite started for UserVoiceSite {0} to RavenDbServer {1}", site.Name, ravenDbServer);
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            var client = new UserVoice.Client(site.Subdomain, API_KEY);
+
+            var suggestions = client.GetCollection("/api/v1/forums/" + site.Id + "/suggestions");
+
+            var totalNumber = suggestions.Count;
+
+            Console.WriteLine("Total suggestions: " + totalNumber);
+
+            using (var documentStore = new DocumentStore { Url = ravenDbServer })
+            {
+                documentStore.Initialize();
+
+                using (var bulkInsert = documentStore.BulkInsert(options: new BulkInsertOptions() { CheckForUpdates = true, BatchSize = 100 }))
+                {
+                    int i = 0;
+
+                    foreach (var suggestionJson in suggestions)
+                    {
+                        int percentage = 0;
+
+                        if(i > 0)
+                        {
+                            percentage =  i * 100/totalNumber;
+                        }
+
+                        Console.WriteLine("Completed {0}% (Counter: {1})", percentage, i);
+
+                        var suggestionObject = suggestionJson.ToObject<Suggestion>();
+                        suggestionObject.Site = site;
+                        bulkInsert.Store(suggestionObject);
+                        i++;
+                    }
+                }
+            }
+
+            watch.Stop();
+
+            Console.WriteLine("Job for UserVoiceSite {0} finished. Elpased Time: {1}:{2}", site.Name, watch.Elapsed.Minutes, watch.Elapsed.Seconds);
+
+        }
+    }
+
+
+
+}
---
+++ BecauseWeCare.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BecauseWeCare.Web")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BecauseWeCare.Web")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("39381ab9-e3e1-4d34-9d9d-f784ab06d949")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
